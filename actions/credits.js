e239d6b42cdc7005a150f9fc77e0a8f0482aeaa5
[bisha21] <[timilsinab22bi@gmail.com]>
2025-06-07 07:23:26 +0545

    first commit

---
+++ actions/credits.js
@@ -0,0 +1,90 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { format } from "date-fns";
+import { revalidatePath } from "next/cache";
+
+const PLAN_CREDITS = {
+    free_user: 0,
+    standard: 10,
+    premium: 24
+}
+const APPOINTMENT_CREDIT_COST = 2;
+export async function checkAndAllocateCredits(user) {
+
+    try {
+        if (!user) return null;
+
+        if (user.role !== 'PATIENT') return user;
+
+        const { has } = await auth();
+        const hasBasic = has({ plan: "free_user" });
+        const hasStandard = has({ plan: "standard" });
+        const hasPremium = has({ plan: "premium" });
+
+        let currentPlan = null;
+        let creditsToAllocate = 0;
+
+        if (hasPremium) {
+            currentPlan = "premium";
+            creditsToAllocate = PLAN_CREDITS.premium;
+        } else if (hasStandard) {
+            currentPlan = "standard";
+            creditsToAllocate = PLAN_CREDITS.standard;
+        } else if (hasBasic) {
+            currentPlan = "free_user";
+            creditsToAllocate = PLAN_CREDITS.free_user;
+        }
+
+        if (!currentPlan) {
+            return user;
+        }
+
+        const currentMonth = format(new Date(), 'yyyy-MM');
+        if (user.transactions.length > 0) {
+            const latestTransaction = user.transactions[0];
+            const transactionMonth = format(latestTransaction.createdAt, 'yyyy-MM');
+            const transactionPlan = latestTransaction.packageId;
+            if (
+                transactionMonth === currentMonth &&
+                transactionPlan === currentPlan
+            ) {
+                return user;
+            }
+        }
+        const updatedUser = await db.$transaction(async (tx) => {
+            // Create transaction record
+            await tx.creditTransaction.create({
+                data: {
+                    userId: user.id,
+                    amount: creditsToAllocate,
+                    type: "CREDIT_PURCHASE",
+                    packageId: currentPlan,
+                },
+            });
+
+            // Update user's credit balance
+            const updatedUser = await tx.user.update({
+                where: {
+                    id: user.id,
+                },
+                data: {
+                    credits: {
+                        increment: creditsToAllocate,
+                    },
+                },
+            });
+
+            return updatedUser;
+        });
+        revalidatePath('/doctors');
+        revalidatePath('/appointments');
+
+        return updatedUser
+
+
+    } catch (e) {
+        console.log(e);
+    }
+}
